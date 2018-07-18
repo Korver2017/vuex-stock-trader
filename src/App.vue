ec39efd7db26da81b7bdc52d67495ace97455a8b
Korver <korver2017@gmail.com>
2018-07-18 21:25:36 +0800

    add animation to router

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <app-header></app-header>
     <div class="col-xs-12">
+      <transition name="slide" mode="out-in">
       <router-view/>
+      </transition>
     </div>
   </div>
 </template>
@@ -24,5 +26,33 @@ export default {
 <style>
 body {
   padding: 30px;
+}
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
 }
 </style>
